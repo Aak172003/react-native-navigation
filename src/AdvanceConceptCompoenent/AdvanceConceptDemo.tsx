2f35d7511f6a736c2bd3a23fc2da986e9b44d765
Aakashmoondive502 <aakash.prajapati@moondive.co>
2025-05-01 00:57:13 +0530

    render-touchablebutton-to-redirect-particular-page

--- src/AdvanceConceptCompoenent/AdvanceConceptDemo.tsx
+++ src/AdvanceConceptCompoenent/AdvanceConceptDemo.tsx
@@ -1,8 +1,22 @@
-import { View, Text, Button } from "react-native";
+import { View, Text, Button, FlatList, TouchableOpacity } from "react-native";
 import React, { FC } from "react";
 import { StackNavigationProp } from "@react-navigation/stack";
 import { AdvanceConceptsParamsList } from "../screens/AdvanceConcepts";
 import { useNavigation } from "@react-navigation/native";
+// import { useNavigation } from "@react-navigation/native";
+
+const topics = [
+  {
+    id: 1,
+    title: "FlatListDemo",
+    route: "FlatListDemo",
+  },
+  {
+    id: 2,
+    title: "FlatListDemo",
+    route: "FlatListDemo",
+  },
+];
 
 // It contains params list and current page name
 type AdvanceConceptDemoProps = StackNavigationProp<
@@ -10,17 +24,40 @@ type AdvanceConceptDemoProps = StackNavigationProp<
   "AdvanceConceptDemo"
 >;
 
-const AdvanceConceptDemo: FC = () => {
-  const navigation = useNavigation<AdvanceConceptDemoProps>();
+type props = {
+  navigation: AdvanceConceptDemoProps;
+};
+
+const AdvanceConceptDemo: FC<props> = ({ navigation }) => {
+  const stackNavigation = useNavigation<AdvanceConceptDemoProps>();
 
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text style={{ fontSize: 20, fontWeight: "bold" }}>
         AdvanceConceptDemo
       </Text>
+{/* 
       <Button
         title="FlatListDemo"
-        onPress={() => navigation.navigate("FlatListDemo")}
+        onPress={() => stackNavigation.navigate("FlatListDemo")}
+      /> */}
+
+      <FlatList
+        data={topics}
+        renderItem={({ item }) => (
+          <TouchableOpacity
+            style={{
+              padding: 10,
+              borderWidth: 1,
+              borderColor: "black",
+              margin: 10,
+              borderRadius: 10,
+            }}
+            onPress={() => navigation.navigate(item.route as never)}
+          >
+            <Text>{item.title}</Text>
+          </TouchableOpacity>
+        )}
       />
     </View>
   );
